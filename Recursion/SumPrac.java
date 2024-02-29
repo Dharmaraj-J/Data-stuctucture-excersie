12edd879d1802f00cfb6661155303fc9de141482
Dharmarj-J <dharmaraj200303@gmail.com>
2024-02-28 21:38:57 -1200

    first commit

---
+++ Recursion/SumPrac.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+// Sum of N numbers
+
+public class SumPrac {
+
+    public static int sum(int n) {
+        if (n == 1)
+            return 1;
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int result = sum(5);
+        System.out.println(result);
+    }
+}
